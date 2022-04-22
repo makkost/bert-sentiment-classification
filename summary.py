a4bbe67a366cf0d6ef867d71292d4fb553bb9f9a
Maksim Kosterin <maksim.kosterin@fruct.org>
2022-04-22 10:57:55 +0300

    Add model files

--- summary.py
+++ summary.py
@@ -2,19 +2,20 @@ import sys
 import os
 import numpy as np
 from functools import reduce
-from sklearn.metrics import precision_recall_fscore_support
+from sklearn.metrics import precision_recall_fscore_support, confusion_matrix
 from parse_args import parse_arguments
+import pandas as pd
 
-LABELS = ["negative", "mixed", "neutral", "positive"]
+LABELS = ["neutral", "positive", "negative", "mixed"]
 
 def get_prediction(predictions):
-    if max(predictions) == predictions[0]:
+    if max(predictions) == predictions[2]:
         return 'neutral'    
-    elif max(predictions) == predictions[1]:
+    elif max(predictions) == predictions[0]:
         return 'negative'
-    elif max(predictions) == predictions[2]:
-        return 'positive'
     elif max(predictions) == predictions[3]:
+        return 'positive'
+    elif max(predictions) == predictions[1]:
         return 'mixed'
 
 def summarize(args):
@@ -58,6 +59,44 @@ def summarize(args):
         f.write("{:10}{:10}{:10}{:10}{:10}\n".format("macro", *np.around(macro, 2), len(data)))
         weighted = np.array(reduce(np.add, [sub["weighted"] for sub in totals])) / split
         f.write("{:10}{:10}{:10}{:10}{:10}\n".format("weighted", *np.around(weighted, 2), len(data)))
+
+def confusion(args):
+    totals = np.zeros((len(LABELS), len(LABELS)))
+    split = args.split
+    for i in range(0, split):
+        dataset = "{}/set{}".format(*args.path, i)
+        data = []
+        with open("{}/test.tsv".format(dataset), encoding = 'utf-8', mode = 'r') as f:
+            data = [{"text": line.strip().split("\t")[0], "label": line.strip().split("\t")[1]} for line in f.readlines()[1:]]
+
+        with open("{}/output/test_results.tsv".format(dataset), encoding = 'utf-8', mode = 'r') as f:
+            for num, line in enumerate(f.readlines(), start=0):
+                data[num]["prediction"] = get_prediction(line.strip().split("\t"))
+
+        y_true = np.array([entry["label"] for entry in data])
+        y_pred = np.array([entry["prediction"] for entry in data])
+        
+        with open("{}/output/confusion.txt".format(dataset), encoding = 'utf-8', mode = 'w') as f:
+            labeled = confusion_matrix(y_true, y_pred, labels=LABELS)
+            print(labeled)
+            f.write("\tpredicted\n")
+            f.write(" \t{}\t{}\t{}\t{}\n".format(*LABELS))
+            for num, label in enumerate(LABELS, start=0):
+                f.write("{}\t{}\t{}\t{}\t{}\n".format(label, *labeled[num]))
+
+            totals = totals + labeled
+        with open("{}/output/errors.txt".format(dataset), encoding = 'utf-8', mode = 'w') as f:
+            f.write("text\tlabel\tprediction\n")
+            for line in data:
+                if line["label"] != line["prediction"]:
+                    f.write("{}\t{}\t{}\n".format(line["text"], line["label"], line["prediction"]))
+    
+    with open("{}/total_confusion.txt".format(*args.path), encoding = 'utf-8', mode = 'w') as f:
+        f.write("\tpredicted\n")
+        f.write(" \t{}\t{}\t{}\t{}\n".format(*LABELS))
+        for num, label in enumerate(LABELS, start=0):
+            f.write("{}\t{}\t{}\t{}\t{}\n".format(label, *np.around(totals[num] / 472.0, 2)))
+
 
 if __name__ == '__main__':
     arguments = parse_arguments()
--- summary2.py
+++ summary2.py
@@ -3,10 +3,13 @@ import os
 import re
 import numpy as np
 from functools import reduce
-from sklearn.metrics import precision_recall_fscore_support, classification_report
+from sklearn.metrics import precision_recall_fscore_support, confusion_matrix
 from parse_args import parse_arguments
 
-LABELS = ['0', '2', '1', '3']
+LABELS = ["neutral", "positive", "negative", "mixed"]
+
+def get_label(num):
+    return LABELS[int(num)]
 
 def summarize(args):
     totals = []
@@ -17,7 +20,7 @@ def summarize(args):
         with open("{}/output/test_results.tsv".format(dataset), encoding = 'utf-8', mode = 'r') as f:
             for num, line in enumerate(f.readlines(), start=0):
                 sent, pred = line.strip().split("\t")
-                data.append({"label": sent, "prediction": pred})
+                data.append({"label": get_label(sent), "prediction": get_label(pred)})
 
         y_true = np.array([entry["label"] for entry in data])
         y_pred = np.array([entry["prediction"] for entry in data])
@@ -47,6 +50,51 @@ def summarize(args):
         f.write("{:10}{:10}{:10}{:10}{:10}\n".format("macro", *np.around(macro, 2), len(data)))
         weighted = np.array(reduce(np.add, [sub["weighted"] for sub in totals])) / split
         f.write("{:10}{:10}{:10}{:10}{:10}\n".format("weighted", *np.around(weighted, 2), len(data)))
+
+def confusion(args):
+    totals = np.zeros((len(LABELS), len(LABELS)))
+    split = args.split
+    for i in range(0, split):
+        dataset = "{}/set{}".format(*args.path, i)
+        data = []
+        # with open("{}/test.tsv".format(dataset), encoding = 'utf-8', mode = 'r') as f:
+        #     data = [{"text": line.strip().split("\t")[0]} for line in f.readlines()[1:]]
+
+        with open("{}/test_result.tsv".format(dataset), encoding = 'utf-8', mode = 'r') as f:
+            for num, line in enumerate(f.readlines(), start=0):
+                sent, pred = line.strip().split("\t")
+                data.append({"label": get_label(sent), "prediction": get_label(pred)})
+
+        # with open("{}/output/test_results.tsv".format(dataset), encoding = 'utf-8', mode = 'r') as f:
+        #     for num, line in enumerate(f.readlines(), start=0):
+        #         sent, pred = line.strip().split("\t")
+        #         data[num]["label"] = get_label(sent)
+        #         data[num]["prediction"] = get_label(pred)
+
+        y_true = np.array([entry["label"] for entry in data])
+        y_pred = np.array([entry["prediction"] for entry in data])
+        
+        with open("{}/confusion.txt".format(dataset), encoding = 'utf-8', mode = 'w') as f:
+            labeled = confusion_matrix(y_true, y_pred, labels=LABELS)
+            print(labeled)
+            f.write("\tpredicted\n")
+            f.write(" \t{}\t{}\t{}\t{}\n".format(*LABELS))
+            for num, label in enumerate(LABELS, start=0):
+                f.write("{}\t{}\t{}\t{}\t{}\n".format(label, *labeled[num]))
+
+            totals = totals + labeled
+        
+        # with open("{}/output/errors.txt".format(dataset), encoding = 'utf-8', mode = 'w') as f:
+        #     f.write("text\tlabel\tprediction\n")
+        #     for line in data:
+        #         if line["label"] != line["prediction"]:
+        #             f.write("{}\t{}\t{}\n".format(line["text"], line["label"], line["prediction"]))
+    print(totals)
+    with open("{}/total_confusion.txt".format(*args.path), encoding = 'utf-8', mode = 'w') as f:
+        f.write("\tpredicted\n")
+        f.write(" \t{}\t{}\t{}\t{}\n".format(*LABELS))
+        for num, label in enumerate(LABELS, start=0):
+            f.write("{}\t{}\t{}\t{}\t{}\n".format(label, *np.around(totals[num] / 424.0, 2)))
 
 if __name__ == '__main__':
     arguments = parse_arguments()
